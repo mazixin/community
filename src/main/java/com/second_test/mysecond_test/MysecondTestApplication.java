311a64bec03660f7ea151adc3d51ee5e92e5c1f9
mazixin <694179453@qq.com>
2019-09-03 22:11:24 +0800

    init repo

---
+++ src/main/java/com/second_test/mysecond_test/MysecondTestApplication.java
@@ -0,0 +1,13 @@
+package com.second_test.mysecond_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecondTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysecondTestApplication.class, args);
+    }
+
+}
