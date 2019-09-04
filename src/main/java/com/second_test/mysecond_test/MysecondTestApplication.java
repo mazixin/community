f9822b8ec76313f666c4bcc81fe080b25df0d8c8
mazixin <694179453@qq.com>
2019-09-04 08:51:48 +0800

    add index and pom

--- src/main/java/com/second_test/mysecond_test/MysecondTestApplication.java
+++ src/main/java/com/second_test/mysecond_test/MysecondTestApplication.java
@@ -2,8 +2,9 @@ package com.second_test.mysecond_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MysecondTestApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/second_test/mysecond_test/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.second_test.mysecond_test.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
